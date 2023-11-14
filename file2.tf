56c0fd8526f0ddc5e96c7bd42f6e4ccfd43eb7a8
Nikitas-git <142219921+Nikitas-git@users.noreply.github.com>
2023-11-14 17:53:50 +0530

    Update file2.tf

--- file2.tf
+++ file2.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAYGINE7OCXPE"
-  secret_key = "VsN//7GRl4j2n4nDgKutUqVkWd"
+  access_key = "AKIAYGIN7OXPE"
+  secret_key = "VsN//7GRl4n4nDgKutUqVkWd"
 }
 
 resource "aws_instance" "terraform_instance1" {
